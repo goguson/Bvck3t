7d9ade3ee60beb36230d784db912b27ccdd6b49a
Your Name <d.s.gogola@gmail.com>
2024-03-06 22:20:50 +0100

    chore: trick2commit3 gonna start working soon

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ fn main() {
     println!("Hello, world2! lece w kulki yo");
     println!("Hello, world111 gonna start working on project soon");
     println!("Hello, world111 gonna start working on project soon2");
+    println!("Hello, world111 gonna start working on project soon3");
 }
